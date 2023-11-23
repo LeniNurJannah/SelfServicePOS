7a8ab3d05b985f505cba46175b39300c852d1e2e
LeniNurJannah <leninurjannah2107@gmail.com>
2023-11-23 10:25:39 +0700

    commit kedua

---
+++ database/seeders/EmployeeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('employees')->insert([
+            'nama' => 'Warsa',
+            'jeniskelamin' => 'cowo',
+            'notelpon' => '089572611852',
+
+        ]);
+    }
+}
